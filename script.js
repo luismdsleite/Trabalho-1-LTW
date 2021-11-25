083d6084d8672a2980f198fcb58e477c0f37d132
luismdsleite <luismdsleite@gmail.com>
2021-11-25 20:59:54 +0000

    removendo funções duplicadas no script e adicionando pop up de configuração

--- script.js
+++ script.js
@@ -1,23 +1,16 @@
 const openPopButton = document.querySelectorAll('[data-pop-target]');
 const closePopButton = document.querySelectorAll('[data-close-button]');
-const overlay = document.getElementById('overlay');	
-const openPop2Button = document.querySelectorAll('[data-pop2-target]');
-const closePop2Button = document.querySelectorAll('[data-close2-button]');
+const overlay = document.getElementById('overlay');
 
+// Listener to open a pop
 openPopButton.forEach(button => {
 	button.addEventListener('click', () => {
 		const pop = document.querySelector(button.dataset.popTarget);
-    openPop(pop);
+		openPop(pop);
 	});
 })
 
-openPop2Button.forEach(button => {
-	button.addEventListener('click', () => {
-		const pop2 = document.querySelector(button.dataset.pop2Target);
-	openPop(pop2);
-	});
-})
-
+// Listener to close all active pops when clicking anywhere outside pop or popheader
 overlay.addEventListener('click', () => {
 	const pops = document.querySelectorAll('.pop.active');
 	pops.forEach(pop => {
@@ -25,28 +18,22 @@ overlay.addEventListener('click', () => {
 	});
 })
 
+// Listener on x button to close pop
 closePopButton.forEach(button => {
-	button.addEventListener('click', () =>{
-		const pop =button.closest('.pop');
-    closePop(pop);
-	});
-})
-
-closePop2Button.forEach(button => {
-	button.addEventListener('click', () =>{
-		const pop2 =button.closest('.pop');
-		closePop(pop2);
+	button.addEventListener('click', () => {
+		const pop = button.closest('.pop');
+		closePop(pop);
 	});
 })
 
 function openPop(pop) {
 	if (pop == null) return;
-  pop.classList.add('active');
-  overlay.classList.add('active');
+	pop.classList.add('active');
+	overlay.classList.add('active');
 }
 
 function closePop(pop) {
 	if (pop == null) return;
-  pop.classList.remove('active');
-  overlay.classList.remove('active');
+	pop.classList.remove('active');
+	overlay.classList.remove('active');
 }
